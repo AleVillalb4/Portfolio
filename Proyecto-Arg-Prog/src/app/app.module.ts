1ccd4430fcfd44f49026f95d2266f53ba177a97a
AleVillalb4 <avillalba793@gmail.com>
2022-07-30 19:06:31 -0300

    componente hasta hys

--- Proyecto-Arg-Prog/src/app/app.module.ts
+++ Proyecto-Arg-Prog/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HysComponent } from './components/hys/hys.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    HysComponent,
+    EducacionComponent,
+    LogoApComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
